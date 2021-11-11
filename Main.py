aa0a6654a4b7b5339e40887e467ebd789b0ece32
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-11-11 10:16:06 -0500

    Adding support for playlist

--- Main.py
+++ Main.py
@@ -1,14 +1,31 @@
 from pytube import YouTube
+from pytube import Playlist
+import re
 
 #where to save
-SAVE_PATH = "C:/MyFolder" 
+SAVE_PATH = "C:/MyVideos" 
 
-#link of the video to be downloaded
+# please note that the playlist must be public
+playlist = Playlist('https://www.youtube.com/playlist?list=PLzwWSJNcZTMSW-v1x6MhHFKkwrGaEgQ-L')
 
 
-#Use a list  ["https://youtu.be/G1233123123123",""https://youtu.be/GhZi12312312354"]
+# this fixes the empty playlist.videos list
+playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-links = []
+print(len(playlist.video_urls))
+
+to_be_downloaded_list = []
+
+for url in playlist.video_urls:
+    print(url)
+    to_be_downloaded_list.append(url)
+
+
+
+# If you have a few videos use the following, otherwise, put the playlist in playlist variable (above)    
+# links = ['https://www.youtube.com/watch?v=xCqbufBv3Lk&list=PLjVtv4TPnrcJ7JAyNbTY3qmNxnIwBqr85&index=144','https://www.youtube.com/watch?v=FemlaDqXxos&list=PLjVtv4TPnrcJ7JAyNbTY3qmNxnIwBqr85&index=141']
+
+links = to_be_downloaded_list
      
 for link in links:
     try:         
@@ -20,11 +37,18 @@ for link in links:
         
         print(f"## This is the ranking of this video: {video.rating}")
         print(f"## This is the duration of this video: {video.length/60} minutes! ")
+        
+        
+       
         video.streams.get_highest_resolution().download(SAVE_PATH) 
         print(f"## Completed Download for: {video.title}") 
     except:
         #to handle exception
         print("Connection Error")
      
-print('All downloads are Completed!')
+print('Task Completed!')
 
+# # physically downloading the audio track
+# for video in playlist.videos:
+#     audioStream = video.streams.get_by_itag(YOUTUBE_STREAM_AUDIO)
+#     audioStream.download(output_path=DOWNLOAD_DIR)
